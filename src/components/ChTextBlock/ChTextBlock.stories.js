8bf8ec410f3a225e0be4a27977e1694dab93903e
Mikhail Petrov <petrov.m@chocolife.kz>
2022-05-13 01:55:09 +0600

    feat(stories): [ChTextBlock] Create storybook stories for ChTextBlock

---
+++ src/components/ChTextBlock/ChTextBlock.stories.js
@@ -0,0 +1,40 @@
+import ChTextBlock from './ChTextBlock.vue'
+
+export default {
+  title: 'TextBlock',
+  component: ChTextBlock,
+  argTypes: {
+    description: { control: 'text' },
+    label: { control: 'text' },
+    caption: { control: 'text' },
+    additional: { control: 'text' }
+  }
+}
+
+const Template = args => ({
+  components: { ChTextBlock },
+  setup() {
+    return { args }
+  },
+  template: '<ch-text-block v-bind="args" />'
+})
+
+export const WithLabel = Template.bind({})
+WithLabel.args = {
+  label: 'Label'
+}
+
+export const WithDescription = Template.bind({})
+WithDescription.args = {
+  description: 'Description',
+  label: 'Label'
+}
+
+export const Full = Template.bind({})
+Full.args = {
+  description: 'Description',
+  label: 'Label',
+  icon: ['fas', 'location'],
+  caption: 'Caption',
+  additional: 'additional'
+}
