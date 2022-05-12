b33cff4506263072706bde45336fcb50077cd931
Mikhail Petrov <petrov.m@chocolife.kz>
2022-05-13 01:48:09 +0600

    feat(stories): [ChCheckbox] Create storybook stories for ChCheckbox

---
+++ src/components/ChCheckbox/ChCheckbox.stories.js
@@ -0,0 +1,37 @@
+import ChCheckbox from './ChCheckbox.vue'
+
+export default {
+  title: 'Checkbox',
+  component: ChCheckbox,
+  argTypes: {
+    checked: { control: 'boolean', defaultValue: false },
+    disabled: { control: 'boolean', defaultValue: false },
+    onClick: {}
+  }
+}
+
+const Template = args => ({
+  components: { ChCheckbox },
+  setup() {
+    return { args }
+  },
+  template: '<ch-checkbox v-bind="args" />'
+})
+
+export const Empty = Template.bind({})
+Empty.args = {
+  label: ''
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  disabled: true,
+  checked: true,
+  label: 'Checkbox'
+}
+
+export const Checked = Template.bind({})
+Checked.args = {
+  checked: true,
+  label: 'Checkbox'
+}
