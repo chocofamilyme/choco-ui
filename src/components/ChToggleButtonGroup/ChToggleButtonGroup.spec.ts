217b059529d4756c18835c83a42dbad3a7708dfa
Mikhail Petrov <petrov.m@chocolife.kz>
2022-05-13 02:55:52 +0600

    fix(tests): [ChToggleButtonGroup] Fix tests

--- src/components/ChToggleButtonGroup/ChToggleButtonGroup.spec.ts
+++ src/components/ChToggleButtonGroup/ChToggleButtonGroup.spec.ts
@@ -1,10 +1,11 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import ChToggleButtonGroup from './ChToggleButtonGroup.vue'
+import { WithLabel } from './ChToggleButtonGroup.stories.js'
 
 describe('ChToggleButtonGroup', () => {
   it('renders properly', () => {
-    const wrapper = mount(ChToggleButtonGroup, { label: 'Hello Vitest' })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(ChToggleButtonGroup, { props: { ...WithLabel.args } })
+    expect(wrapper.html()).toContain('Label 2')
   })
 })
