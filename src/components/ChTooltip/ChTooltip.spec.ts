f4e962882d7a15deafcde5cc9395245603851f6b
Mikhail Petrov <petrov.m@chocolife.kz>
2022-05-13 02:56:05 +0600

    fix(tests): [ChTooltip] Fix tests

--- src/components/ChTooltip/ChTooltip.spec.ts
+++ src/components/ChTooltip/ChTooltip.spec.ts
@@ -3,8 +3,17 @@ import { mount } from '@vue/test-utils'
 import ChTooltip from './ChTooltip.vue'
 
 describe('ChTooltip', () => {
-  it('renders properly', () => {
-    const wrapper = mount(ChTooltip, { label: 'Hello Vitest' })
-    expect(wrapper.text()).toContain('Hello Vitest')
+  it('renders properly', async () => {
+    const wrapper = mount(ChTooltip, {
+      props: {
+        label: 'Hello Vitest',
+        behaviour: 'click'
+      },
+      slots: { default: '<button>button</button>' }
+    })
+
+    await wrapper.find('button').trigger('click')
+
+    expect(wrapper.html()).toContain('Hello Vitest')
   })
 })
