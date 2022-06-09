e3089d1c63696dce2b7c79eb5feae30e93535bfd
Kirill Solovyov <k.solovev@aviata.me>
2022-06-09 18:01:59 +0600

    feat(ChHeader): add ChHeaderCloseButton component

---
+++ src/components/ChHeader/ChHeaderCloseButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    type="button"
+    class="ch-header__default-button ch-header__close-btn"
+    @click="$emit('onCloseButtonClick')"
+  >
+    <img src="@/assets/images/icons/close.svg" width="24" height="24" alt="" />
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ChHeader.CloseButton'
+}
+</script>
+
+<style lang="sass" scoped>
+.ch-header__close-btn
+  margin-left: auto
+</style>
