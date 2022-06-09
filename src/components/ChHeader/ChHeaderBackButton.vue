d10fd1b41d11b3e29cc1ef6b84574adbccbf5494
Kirill Solovyov <k.solovev@aviata.me>
2022-06-09 18:01:36 +0600

    feat(ChHeader): add ChHeaderBackButton component

---
+++ src/components/ChHeader/ChHeaderBackButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <button type="button" class="ch-header__default-button" @click="$emit('onBackButtonClick')">
+    <img src="@/assets/images/icons/arrow-left.svg" width="24" height="24" alt="" />
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ChHeader.BackButton'
+}
+</script>
