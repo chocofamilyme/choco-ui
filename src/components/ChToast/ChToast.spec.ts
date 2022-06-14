1d666dbc966902b96fb74d1399db7c5349067d16
Kirill Solovyov <k.solovev@aviata.me>
2022-06-14 12:45:40 +0600

    test(ChToast): move toast content to variable

--- src/components/ChToast/ChToast.spec.ts
+++ src/components/ChToast/ChToast.spec.ts
@@ -2,9 +2,11 @@ import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import ChToast from './ChToast.vue'
 
+const toastContent = 'Toast content'
+
 describe('ChToast', () => {
   it("should correctly display slot's content", () => {
-    const wrapper = mount(ChToast, { slots: { default: 'Toast content' } })
-    expect(wrapper.text()).toContain('Toast content')
+    const wrapper = mount(ChToast, { slots: { default: toastContent } })
+    expect(wrapper.text()).toContain(toastContent)
   })
 })
