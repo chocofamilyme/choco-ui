7eb1b504f065ba1e7ce375d2ec27e1969c980474
Kirill Solovyov <k.solovev@aviata.me>
2022-08-20 21:23:10 +0600

    feat(ChTabbar): add ChTabbarChocoItem component

---
+++ src/components/ChTabbar/ChTabbarChocoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <ChTabbarItem is-active>
+    <picture>
+      <source type="image/webp" srcset="@/assets/images/icons/choco-icon.webp" />
+      <source type="image/png" srcset="@/assets/images/icons/choco-icon.png" />
+      <img
+        src="@/assets/images/icons/choco-icon.png"
+        width="36"
+        height="36"
+        alt="Вернуться на главную страницу Choco"
+      />
+    </picture>
+  </ChTabbarItem>
+</template>
+
+<script setup lang="ts">
+import ChTabbarItem from './ChTabbarItem.vue'
+</script>
