01fea247b0b47ec4e67b0900bb2499802e2b0194
Kirill Solovyov <k.solovev@aviata.me>
2022-08-20 21:30:07 +0600

    chore(ChTabbar): add exports of ChTabbar components

---
+++ src/components/ChTabbar/index.ts
@@ -0,0 +1,3 @@
+export { default as ChTabbar } from './ChTabbar.vue'
+export { default as ChTabbarItem } from './ChTabbarItem.vue'
+export { default as ChTabbarChocoItem } from './ChTabbarChocoItem.vue'
