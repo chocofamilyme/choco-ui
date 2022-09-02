4c382ab8f97f3df9668677390caff799475cb61f
kate-timofeyeva <e.timofeeva@aviata.me>
2022-09-02 23:44:52 +0600

    test(ChSearchInput): add tests to ChSearchInput

---
+++ src/components/ChSearchInput/ChSearchInput.spec.ts
@@ -0,0 +1,68 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { mount } from '@vue/test-utils'
+import type { VueWrapper } from '@vue/test-utils'
+import ChSearchInput from './ChSearchInput.vue'
+
+let SearchInput: VueWrapper | null = null
+
+describe('ChSearchInput', () => {
+  beforeEach(async () => {
+    SearchInput = mount(ChSearchInput, {
+      props: {
+        modelValue: 'Value',
+        placeholder: 'Поиск',
+        hasShadow: true
+      },
+      slots: {
+        prepend: `<template #prepend>prepend</template>`,
+        append: `<template #append>append</template>`,
+        clearButton: `<template #clearButton>clearButton</template>`
+      }
+    }) as VueWrapper
+  })
+
+  it('should emit "onInput" event with right value, when change value in input', async () => {
+    SearchInput?.find('input[type="text"]').setValue('my value')
+
+    await SearchInput?.vm.$nextTick()
+    expect(SearchInput?.emitted().onInput).toBeTruthy()
+    expect(SearchInput?.emitted().onInput.length).toBe(1)
+    expect(SearchInput?.emitted().onInput[0]).toEqual(['my value'])
+  })
+
+  it('should show cancel button, when focus on input', async () => {
+    expect(SearchInput?.find('.ch-search-input__button').exists()).toBe(false)
+
+    SearchInput?.find('input[type="text"]').trigger('focus')
+
+    await SearchInput?.vm.$nextTick()
+    expect(SearchInput?.find('.ch-search-input__button').exists()).toBe(true)
+  })
+
+  it('should emit "onCancel" event, when click to cancel button', async () => {
+    SearchInput?.find('input[type="text"]').trigger('focus')
+
+    await SearchInput?.vm.$nextTick()
+    SearchInput?.find('.ch-search-input__button').trigger('click')
+
+    await SearchInput?.vm.$nextTick()
+    expect(SearchInput?.emitted().onCancel).toBeTruthy()
+  })
+
+  it('should hide cancel button, when click to cancel button', async () => {
+    SearchInput?.find('input[type="text"]').trigger('focus')
+
+    await SearchInput?.vm.$nextTick()
+    expect(SearchInput?.find('.ch-search-input__button').exists()).toBe(true)
+    SearchInput?.find('.ch-search-input__button').trigger('click')
+
+    await SearchInput?.vm.$nextTick()
+    expect(SearchInput?.find('.ch-search-input__button').exists()).toBe(false)
+  })
+
+  it('should render all slots', async () => {
+    expect(SearchInput?.html()).toContain('prepend')
+    expect(SearchInput?.html()).toContain('append')
+    expect(SearchInput?.html()).toContain('clearButton')
+  })
+})
