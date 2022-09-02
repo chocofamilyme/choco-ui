1fb8d1dd0727329f208a4f125d2d19a03aaa8b81
kate-timofeyeva <e.timofeeva@aviata.me>
2022-09-02 23:41:10 +0600

    feat(ChSearchInput): add сancel button

--- src/components/ChSearchInput/ChSearchInput.vue
+++ src/components/ChSearchInput/ChSearchInput.vue
@@ -7,12 +7,27 @@
       class="ch-search-input__input"
       :class="{ 'ch-search-input__input_with-shadow': hasShadow }"
       @update:modelValue="emit('onInput', $event)"
+      @focus="onFocus"
+      @blur="onBlur"
     />
+    <Transition name="сancel-button-leave">
+      <ChButton
+        v-if="isVisibleCancelButton"
+        type="button"
+        size="xs"
+        simple
+        class="ch-search-input__button"
+        @click="onCancel"
+      >
+        Отмена
+      </ChButton>
+    </Transition>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ChInput } from '../../'
+import { ref } from 'vue'
+import { ChInput, ChButton } from '../../'
 
 const props = defineProps({
   modelValue: String,
@@ -20,7 +35,21 @@ const props = defineProps({
   hasShadow: Boolean
 })
 
-const emit = defineEmits(['onInput'])
+const emit = defineEmits(['onInput', 'onCancel'])
+const isVisibleCancelButton = ref(false)
+
+function onFocus() {
+  isVisibleCancelButton.value = true
+}
+function onBlur() {
+  if (props.modelValue === '') {
+    isVisibleCancelButton.value = false
+  }
+}
+function onCancel() {
+  isVisibleCancelButton.value = false
+  emit('onCancel')
+}
 </script>
 
 <script lang="ts">
@@ -44,4 +73,23 @@ export default defineComponent({
 
     &_with-shadow
       box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.08), -1px -1px 10px rgba(0, 0, 0, 0.08)
+
+  &__button
+    padding: 0
+    width: 68px
+    justify-content: end
+    color: var(--color-secondary-text)
+    overflow: hidden
+
+.сancel-button-leave
+  &-enter-active,
+  &-leave-active
+    transition: all 0.5s ease
+    width: 68px
+    opacity: 1
+
+  &-enter-from,
+  &-leave-to
+    width: 0px
+    opacity: 0
 </style>
