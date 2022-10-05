88ddcce862dc33a56c83afd693d960e6cb8f083d
Kirill Solovyov <k.solovev@aviata.me>
2022-10-05 15:00:52 +0600

    test(ChInput): add type props tests

--- src/components/ChInput/ChInput.spec.ts
+++ src/components/ChInput/ChInput.spec.ts
@@ -5,6 +5,8 @@ import type { VueWrapper } from '@vue/test-utils'
 import { findByTestId } from '@/__test__/utils'
 import ChInput from './ChInput.vue'
 
+const findInput = (wrapper: VueWrapper) => findByTestId(wrapper, 'ch-input-input-component')
+
 describe('ChInput', () => {
   it('renders properly', () => {
     const wrapper = mount(ChInput, { props: { label: 'Hello Vitest' } })
@@ -13,15 +15,21 @@ describe('ChInput', () => {
 
   it('should be input by default', () => {
     const wrapper = mount(ChInput)
-    expect(findByTestId(wrapper as VueWrapper, 'ch-input-input-component').element.tagName).toBe(
-      'INPUT'
-    )
+    expect(findInput(wrapper as VueWrapper).element.tagName).toBe('INPUT')
   })
 
   it('should be textarea', () => {
     const wrapper = mount(ChInput, { props: { component: 'textarea' } })
-    expect(findByTestId(wrapper as VueWrapper, 'ch-input-input-component').element.tagName).toBe(
-      'TEXTAREA'
-    )
+    expect(findInput(wrapper as VueWrapper).element.tagName).toBe('TEXTAREA')
+  })
+
+  it('should be input of type "text" by default', () => {
+    const wrapper = mount(ChInput)
+    expect(findInput(wrapper as VueWrapper).element.getAttribute('type')).toBe('text')
+  })
+
+  it('should be input of type "number"', () => {
+    const wrapper = mount(ChInput, { props: { type: 'number' } })
+    expect(findInput(wrapper as VueWrapper).element.getAttribute('type')).toBe('number')
   })
 })
